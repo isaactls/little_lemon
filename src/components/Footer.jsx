c016faadb2af0c6a77cdf446d45b775a531be9a3
isaactls <isacktolesa@gmail.com>
2024-03-03 03:19:27 -0800

    setting up html semantic structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <img src="" alt="" />
+        <ul>
+          <li>Dormant Navigation</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservation</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+        <ul>
+          <li>Contact</li>
+          <li>Address</li>
+          <li>Phone number</li>
+          <li>Email</li>
+        </ul>
+        <ul>
+            <li>Social Media Link</li>
+            <li>Facebook</li>
+            <li>Insagram</li>
+            <li>Tiktok</li>
+        </ul>
+      </ul>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import Logo from "../assets/images/Logo.svg"
+const Header = () => {
+    return(
+        <header>
+            <img src={Logo} alt="logo" />
+        </header>
+    )
+}
+export default Header;
